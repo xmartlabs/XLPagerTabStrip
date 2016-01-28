c6446c72b940a75a29bbc4d72e824d1a78174db3
Martin Barreto <martin@xmartlabs.com>
2016-01-28 14:53:25 -0300

    make currentIndex and viewCOntrollers public.

--- Sources/PagerTabStripViewController.swift
+++ Sources/PagerTabStripViewController.swift
@@ -64,8 +64,8 @@ public class PagerTabStripViewController: UIViewController, UIScrollViewDelegate
     
     public var pagerOptions = PagerTabStripOptions.SkipIntermediateViewControllers.union(.IsProgressiveIndicator).union(.IsElasticIndicatorLimit)
     
-    private(set) var viewControllers = [UIViewController]()
-    private(set) var currentIndex = 0
+    public private(set) var viewControllers = [UIViewController]()
+    public private(set) var currentIndex = 0
     
     public var pageWidth: CGFloat {
         return CGRectGetWidth(containerView.bounds)
