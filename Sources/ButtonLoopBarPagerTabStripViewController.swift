4c671f3c1d8775ee4fe2b6629ad96f37a0999e71
Toshinori Tsugita <tsugitta@gmail.com>
2016-05-20 06:21:12 +0900

    Create ButtonLoopBarPagerTabStripViewController

---
+++ Sources/ButtonLoopBarPagerTabStripViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonLoopBarPagerTabStripViewController.swift
+//  Pods
+//
+//  Created by tsugita on 2016/05/20.
+//
+//
+
+import UIKit
+
+public class ButtonLoopBarPagerTabStripViewController: ButtonBarPagerTabStripViewController {
+
+}
