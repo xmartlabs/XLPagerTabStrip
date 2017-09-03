354865ff59b87757dacdde6989f920c2aa00361a
saltylight <micaelace@kaist.ac.kr>
2017-09-03 23:55:34 +0900

    added bottom border setting code

--- Sources/BarPagerTabStripViewController.swift
+++ Sources/BarPagerTabStripViewController.swift
@@ -26,48 +26,53 @@ import Foundation
 import UIKit
 
 public struct BarPagerTabStripSettings {
-
+    
     public struct Style {
         public var barBackgroundColor: UIColor?
         public var selectedBarBackgroundColor: UIColor?
         public var barHeight: CGFloat = 5 // barHeight is ony set up when the bar is created programatically and not using storyboards or xib files.
+        public var bottomBorderHeight: CGFloat = 0 // bottomBorderHeight
+        public var bottomBorderColor: UIColor?
     }
-
+    
     public var style = Style()
 }
 
 open class BarPagerTabStripViewController: PagerTabStripViewController, PagerTabStripDataSource, PagerTabStripIsProgressiveDelegate {
-
+    
     public var settings = BarPagerTabStripSettings()
-
+    
     @IBOutlet weak public var barView: BarView!
-
+    
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         delegate = self
         datasource = self
     }
-
+    
     public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         delegate = self
         datasource = self
     }
-
+    
     open override func viewDidLoad() {
         super.viewDidLoad()
         barView = barView ?? {
             let barView = BarView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: settings.style.barHeight))
+            let bottomBorderView = UIView(frame: CGRect(x: 0, y: barView.frame.height - settings.style.bottomBorderHeight, width: barView.frame.width, height: settings.style.bottomBorderHeight))
+            bottomBorderView.backgroundColor = settings.style.bottomBorderColor ?? .blue
+            barView.addSubview(bottomBorderView)
             barView.autoresizingMask = .flexibleWidth
             barView.backgroundColor = .black
             barView.selectedBar.backgroundColor = .white
             return barView
-        }()
-
+            }()
+        
         barView.backgroundColor = settings.style.barBackgroundColor ?? barView.backgroundColor
         barView.selectedBar.backgroundColor = settings.style.selectedBarBackgroundColor ?? barView.selectedBar.backgroundColor
     }
-
+    
     open override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if barView.superview == nil {
@@ -76,22 +81,22 @@ open class BarPagerTabStripViewController: PagerTabStripViewController, PagerTab
         barView.optionsCount = viewControllers.count
         barView.moveTo(index: currentIndex, animated: false)
     }
-
+    
     open override func reloadPagerTabStripView() {
         super.reloadPagerTabStripView()
         barView.optionsCount = viewControllers.count
-        if isViewLoaded {
+        if isViewLoaded{
             barView.moveTo(index: currentIndex, animated: false)
         }
     }
-
+    
     // MARK: - PagerTabStripDelegate
-
+    
     open func updateIndicator(for viewController: PagerTabStripViewController, fromIndex: Int, toIndex: Int, withProgressPercentage progressPercentage: CGFloat, indexWasChanged: Bool) {
-
+        
         barView.move(fromIndex: fromIndex, toIndex: toIndex, progressPercentage: progressPercentage)
     }
-
+    
     open func updateIndicator(for viewController: PagerTabStripViewController, fromIndex: Int, toIndex: Int) {
         barView.moveTo(index: toIndex, animated: true)
     }
