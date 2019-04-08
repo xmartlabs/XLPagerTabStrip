aeef4f6943f2e99a117d4b5e7b5b4418bef5186f
Cihat Gündüz <commits@cihatguenduez.de>
2019-04-08 11:01:58 +0200

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "XLPagerTabStrip",
+    // platforms: [.iOS("9.3")],
+    products: [
+        .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"])
+    ],
+    targets: [
+        .target(
+            name: "XLPagerTabStrip",
+            path: "Sources"
+        )
+    ]
+)
