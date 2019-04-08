c8defd527b9656f623b24354f26064ceb841f35b
Cihat Gündüz <commits@cihatguenduez.de>
2019-04-08 11:15:15 +0200

    [Package.swift] Properly fix mixed language source files issue

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .target(
             name: "XLPagerTabStrip",
             path: "Sources",
-            sources: ["Sources/PagerTabStripViewController.swift"]
+            exclude: ["FXPageControl.h", "FXPageControl.m"]
         )
     ]
 )
