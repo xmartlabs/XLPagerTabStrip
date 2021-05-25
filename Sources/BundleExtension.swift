40d275b36367cfe5fab26bfdac612e9ed6bf5a20
Arpit Williams <arpitwilliams@yahoo.com>
2021-05-25 13:32:11 +0530

    Added bundle extension to load spm bundle.

---
+++ Sources/BundleExtension.swift
@@ -0,0 +1,37 @@
+//
+//  BundleExtension.swift
+//  XLPagerTabStrip
+//
+//  Created by Arpit Williams on 25/05/21.
+//
+
+import class Foundation.Bundle
+
+private class BundleFinder {}
+
+extension Foundation.Bundle {
+    /// Returns the resource bundle associated with the current Swift module.
+    static var module: Bundle? = {
+        let bundleName = "XLPagerTabStrip_XLPagerTabStrip"
+
+        let candidates = [
+            // Bundle should be present here when the package is linked into an App.
+            Bundle.main.resourceURL,
+
+            // Bundle should be present here when the package is linked into a framework.
+            Bundle(for: BundleFinder.self).resourceURL,
+
+            // For command-line tools.
+            Bundle.main.bundleURL
+        ]
+
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        print("unable to find bundle named XLPagerTabStrip_XLPagerTabStrip")
+        return nil
+    }()
+}
