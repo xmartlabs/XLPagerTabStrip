3cea4d505a2137dd3736ea8a523ec13c015138b0
PM Villa <pm@yepngo.com>
2022-09-27 15:38:11 +0900

    added platform to SPM

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "XLPagerTabStrip",
+    platforms: [
+            .iOS(.v13)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
